421ea2fb23e0bb39d6eb6d92f584b10c87b11416
blhafner <brent.hafner@consensys.net>
2025-04-08 08:41:02 -0500

    Delete metamask-state-log-explorer directory

--- metamask-state-log-explorer/src/components/state-log-viewers/index.ts
+++
@@ -1,6 +0,0 @@
-export { TransactionList } from "./transaction-list";
-export { OriginsList } from "./origins-list";
-export { ApprovalsList } from "./approvals-list";
-export { AccountsList } from "./accounts-list";
-export { VariableViewer } from "./variable-viewer";
-export { MobileInfo } from "./mobile-info";
